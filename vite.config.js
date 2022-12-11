654227a2430c74472ff1e7a2d0a220bf3fedff0b
raidas <mrraidas@gmail.com>
2022-12-11 11:45:12 +0200

    fixing vercel bugs

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,22 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
+const crossOriginIsolation = () => ({
+  name: 'configure-server',
+  configureServer(server) {
+    server.middlewares.use((_req, res, next) => {
+      res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+      res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+      next();
+    });
+  }
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    // crossOriginIsolation()
-    {
-      name: "configure-response-headers",
-      configureServer: (server) => {
-        server.middlewares.use((_req, res, next) => {
-          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-          next();
-        });
-      },
-    },
+    crossOriginIsolation()
   ],
   resolve: {
     alias: {
