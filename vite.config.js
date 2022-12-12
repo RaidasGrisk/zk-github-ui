e8aa5e766baf6515d62281a1d27b2c980d1f553b
raidas <mrraidas@gmail.com>
2022-12-12 09:39:32 +0200

    update (sharedArrayBuffer bug)

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
+// in fact using this does nothing
+// it applies to local dev server only
+// and emitting this wil also work ok
+// fails in production only
 const crossOriginIsolation = () => ({
   name: 'configure-server',
   configureServer(server) {
     server.middlewares.use((_req, res, next) => {
       res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
       res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+      res.setHeader("Cross-Origin-Resource-Policy", "same-site");
       next();
     });
   }
